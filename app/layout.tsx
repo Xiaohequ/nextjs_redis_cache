bf54cb6a01464211134a7a71e294b1708e877142
Xiaohe QU <xiaohe.qu@neutronic.fr>
2025-11-14 10:33:45 +0100

    chapitre 15 : adding-metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+        template : '%s | Xiaohe\'s Acme Dashboard',
+        default : 'Dashboard',
+    },
+    description : 'The official Next.js Course Dashbord, built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+};
 
 export default function RootLayout({
   children,
